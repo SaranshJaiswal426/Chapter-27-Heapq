e82b87cc15c776f61fb479f5cac47e68d32a216b
saranshjaiswal426 <Co.2019.sdjaiswal@bitwardha.ac.in>
2023-02-27 23:33:14 +0530

    try heapq

---
+++ Smallest item.py
@@ -0,0 +1,4 @@
+import heapq
+numbers = [10, 4, 2, 100, 20, 50, 32, 200, 150, 8]
+heapq.heapify(numbers)
+print(numbers)
---
+++ largest and smallest item in collection .py
@@ -0,0 +1,4 @@
+import heapq
+numbers = [1, 4, 2, 100, 20, 50, 32, 200, 150, 8]
+print(heapq.nlargest(4, numbers))
+print(heapq.nsmallest(4, numbers)) 
